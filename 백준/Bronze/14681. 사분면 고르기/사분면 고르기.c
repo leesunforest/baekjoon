074796189193e7c9b3fe8ec4f8ba6333bfcb231e
sunforest <153624836+leesunforest@users.noreply.github.com>
2023-12-13 06:27:05 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    int y;
+    scanf("%d %d", &x, &y);
+    if (x > 0 && y > 0)
+        printf("%d", 1); 
+    else if (x < 0 && y > 0)
+        printf("%d", 2); 
+    else if (x < 0 && y < 0)
+        printf("%d", 3); 
+    else
+        printf("%d", 4); 
+}
