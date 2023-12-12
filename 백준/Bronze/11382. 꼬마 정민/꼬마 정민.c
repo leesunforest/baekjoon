88eebea2c7e54ee825f90826d9f79efbd54bb30c
sunforest <153624836+leesunforest@users.noreply.github.com>
2023-12-13 04:08:24 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long int a;
+    long int b;
+    long int c;
+
+    scanf ("%ld %ld %ld", &a, &b, &c);
+    printf ("%ld\n", a + b + c); 
+    return (0);
+}
