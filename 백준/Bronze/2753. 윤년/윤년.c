c749a898b9a093a6c111f0c9adb9646eca477c9d
sunforest <153624836+leesunforest@users.noreply.github.com>
2023-12-13 04:46:50 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int year;
+    scanf("%d", &year);
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
+        printf("%d", 1); 
+    else
+        printf("%d", 0); 
+}
