b185a757b60af6143f0217003afd2a01942ef03f
sunforest <153624836+leesunforest@users.noreply.github.com>
2023-12-13 04:03:08 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+
+    scanf ("%d %d", &a, &b);
+    printf ("%d\n", (b % 10) * a); 
+    printf ("%d\n", ((b % 100) / 10) * a);
+    printf ("%d\n", (b / 100) * a);
+    printf ("%d\n", a * b); 
+    return (0);
+}
