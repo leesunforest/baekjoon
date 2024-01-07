2b42d356307dc214147140c272951848ec35d0bf
sunforest <153624836+leesunforest@users.noreply.github.com>
2024-01-07 19:06:57 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,11 +1,15 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String id = sc.next();
-        System.out.println(id+"??!");
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
  
-        sc.close();
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String lastString = "??!"; 
+		
+		String s = br.readLine();
+		System.out.println(s + lastString);
+	}
 }
