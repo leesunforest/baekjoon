5efd381b277c2f598af4b3162124dfee76662efb
sunforest <153624836+leesunforest@users.noreply.github.com>
2024-01-12 14:12:51 +0900

    Update 오븐 시계.java

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -6,8 +6,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt(), m = sc.nextInt(), t = sc.nextInt();
 
-        int min = 60 * h + m;
-        min += t;
+        int min = 60 * h + m; // ~시 ~분을 ~분으로 환산
+        min += t; // 몇 분 걸렸는지 더한다.
 
         int hour = (min / 60) % 24;
         int minute = min % 60;
