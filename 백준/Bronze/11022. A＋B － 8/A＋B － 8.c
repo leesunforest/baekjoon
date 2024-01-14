e46c7b20b09ba15e2b790050fd9bdd4df517e680
sunforest <153624836+leesunforest@users.noreply.github.com>
2024-01-14 12:56:28 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    int i = 1;
+    while (i <= t)
+    {
+        int a,b;
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+        ++i;
+    }
+}
