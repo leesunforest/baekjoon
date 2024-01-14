c93e3e8f07b18057eda8bcbaffa3045dc268915a
sunforest <153624836+leesunforest@users.noreply.github.com>
2024-01-14 12:49:20 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 356 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    int i = 0;
+    while (i < t)
+    {
+        int a,b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+        ++i;
+    }
+}
