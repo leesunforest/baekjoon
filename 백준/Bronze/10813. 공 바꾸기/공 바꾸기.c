d583734c379b4a90f099db72774f64111466a988
sunforest <153624836+leesunforest@users.noreply.github.com>
2024-01-15 17:50:17 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.c
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.c
@@ -3,26 +3,23 @@
 int main()
 {
     int n, m;
-    int i, j, temp, a, b;
+    int i, j, temp, a;
     int arr[101];
 
-    scanf("%d %d", &n, &m); //바구니 개수n, 바구니 위치 바꾸는 횟수m
+    scanf("%d %d", &n, &m);
 
     for (a = 1; a <= n; ++a)
     {
-        arr[a] = a; //각 바구니에는 바구니 번호랑 맞는 번호가 들어감.
+        arr[a] = a;
     }
-    for (a = 0; a < m; ++a) //m번 위치교환
+    for (a = 0; a < m; ++a)
     {
-        scanf("%d %d", &i, &j); //위치를 바꿀 두 바구니의 번호
+        scanf("%d %d", &i, &j);
 
-        //swap을 이용해 임시변수 temp에 담아서 두 바구니 위치 교환
         temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-
-    //최종적으로 바구니에 담긴 번호 출력
     for (a = 1; a <= n; ++a)
     {
         printf("%d ", arr[a]);
